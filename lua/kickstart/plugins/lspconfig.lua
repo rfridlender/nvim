88661b83ed1e8db18277ab8d00a155bf1b1b853d
Robert Fridlender <robertfridlender@gmail.com>
2024-08-16 01:47:11 -0400

    Add LSPs

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -167,9 +167,13 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        intelephense = {},
+        gopls = {},
+        perlnavigator = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
+        ['typescript-language-server'] = {},
+        ['yaml-language-server'] = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
